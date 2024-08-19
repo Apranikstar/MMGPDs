abe7008ee110f48333b4ff919bac114400fd2341
Apranik Fatehi <147948498+Apranikstar@users.noreply.github.com>
2024-08-19 13:00:33 +0330

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.GPD import mainGPDs as gpd
 from src.PDF import mainPDFs as pdf
 
 def main():
-    start = input("To use PDFs write: PDF \n To use GPDs write: GPD \n ")
+    start = input("To use PDFs write: PDF \n To use GPDs write: GPD \n")
     if start == "PDF":
         pdf.PDFMethods()
     elif start == "GPD":
